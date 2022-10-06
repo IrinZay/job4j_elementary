1b11c2523ffbb92d22e70eab0e55e9a8c8fd1805
IrinZay <i_m_k@vk.ru>
2022-10-06 10:46:44 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public class StudentInfo {
+public class StudentInfo{
     public static void main (String[] args){
         System.out.println("Irina Zayats");
         System.out.println("18.11.1990");
