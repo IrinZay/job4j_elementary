a938deeaeb015afd8805dcb8187a66f98c90dc43
IrinZay <i_m_k@vk.ru>
2022-11-22 10:30:34 +0300

    5.6. Простые числа [#156314]

--- src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -12,21 +12,21 @@ public class PrimeNumberTest {
         int result = PrimeNumber.calc(finish);
         int expected = 3;
         assertEquals(expected, result);
-        }
-
-        @Test
-        public void when11() {
-            int finish = 11;
-            int result = PrimeNumber.calc(finish);
-            int expected = 5;
-            assertEquals(expected, result);
-        }
-
-        @Test
-        public void when2() {
-            int finish = 2;
-            int result = PrimeNumber.calc(finish);
-            int expected = 1;
-            assertEquals(expected, result);
-        }
     }
+
+    @Test
+    public void when11() {
+        int finish = 11;
+        int result = PrimeNumber.calc(finish);
+        int expected = 5;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void when2() {
+        int finish = 2;
+        int result = PrimeNumber.calc(finish);
+        int expected = 1;
+        assertEquals(expected, result);
+    }
+}
